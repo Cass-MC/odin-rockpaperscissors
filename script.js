846a9bc6f748b916d1368107d95107ee574bf316
Cassandra Cawley <cassandrabook09@gmail.com>
2022-09-13 14:21:53 -0700

    added playRound function

--- script.js
+++ script.js
@@ -13,3 +13,43 @@ function getComputerChoice() {
             break;
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection == "rock") {
+
+        if (computerSelection == "Rock") {
+            return "Try again!";
+        }
+        else if (computerSelection == "Paper") {
+            return "You lose! Paper beats rock!";
+        }
+        else {
+            return "You win! Rock beats scissors!";
+        }
+    }
+    else if (playerSelection == "paper") {
+
+        if (computerSelection == "Rock") {
+            return "You win! Paper beats rock!";
+        }       
+        else if (computerSelection == "Paper") {
+            return "Try again!";
+        }
+        else {
+            return "You lose! Scissors beats paper!";
+        }
+    }
+    else {
+        if (computerSelection == "Rock") {
+            return "You lose! Rock beats scissors!";
+        }
+        else if (computerSelection == "Paper") {
+            return "You win! Scissors beats paper!";
+        }
+        else {
+            return "Try again!";
+        }
+    }
+}
