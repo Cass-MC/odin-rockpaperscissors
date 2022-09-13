6e68dc7b05333b6c6c587daf2b18be286cb5e038
Cassandra Cawley <cassandrabook09@gmail.com>
2022-09-13 14:37:57 -0700

    Added game function

--- script.js
+++ script.js
@@ -17,39 +17,73 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
+    //Compare player's choice to both rock, paper, and scissors- then return who won
     if (playerSelection == "rock") {
 
         if (computerSelection == "Rock") {
-            return "Try again!";
+            return "Tie";
         }
         else if (computerSelection == "Paper") {
-            return "You lose! Paper beats rock!";
+            return "Lose";
         }
         else {
-            return "You win! Rock beats scissors!";
+            return "Win";
         }
     }
     else if (playerSelection == "paper") {
 
         if (computerSelection == "Rock") {
-            return "You win! Paper beats rock!";
+            return "Win";
         }       
         else if (computerSelection == "Paper") {
-            return "Try again!";
+            return "Tie";
         }
         else {
-            return "You lose! Scissors beats paper!";
+            return "Lose";
         }
     }
     else {
         if (computerSelection == "Rock") {
-            return "You lose! Rock beats scissors!";
+            return "Lose";
         }
         else if (computerSelection == "Paper") {
-            return "You win! Scissors beats paper!";
+            return "Win";
         }
         else {
-            return "Try again!";
+            return "Tie";
         }
     }
+}
+
+function game() {
+    let playerScore = 0;
+    let compScore = 0;
+
+    //Run 5 rounds
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = getComputerChoice(); //Since I can't type in my own choice just yet
+        let computerSelection = getComputerChoice();
+
+        switch(playRound(playerSelection,computerSelection)) {
+            case "Lose":
+                compScore++;
+                break;
+            case "Win":
+                playerScore++;
+                break;
+        }
+        console.log(playerSelection,computerSelection);
+        console.log(playerScore,compScore);
+    }
+
+    //Print the results
+    if (playerScore > compScore) {
+        console.log("Player wins!");
+    }
+    else if (compScore > playerScore) {
+        console.log("Computer wins!");
+    }
+    else {
+        console.log("It's a tie!");
+    }   
 }
